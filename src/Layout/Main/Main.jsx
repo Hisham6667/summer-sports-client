24ff5316a0c8816bfc74c374a5768e9d49a2a8d1
Hisham6667 <hisham.44678@gmail.com>
2023-06-10 11:15:12 +0600

    project setup and navbar comlete without some polishing

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Pages/Shared/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-screen"></div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.png'
+
+const Navbar = () => {
+  // TODO: update user from firebase
+  const user = false;
+
+  return (
+    <div className="navbar flex justify-between items-center my-2">
+
+      <div className='flex items-center'>
+        <img className='h-16 w-16' src={logo} alt="" />
+        <div className="flex flex-col justify-center items-center">
+          <p className='uppercase text-4xl font-serif font-semibold text-amber-900'>summer</p>
+          <p className='text-xl text-teal-600'>Sports</p>
+        </div>
+      </div>
+
+      {/* update links to a valid route */}
+      <div className="navbar-center">
+        <ul className="menu menu-horizontal px-1 text-xl">
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/'>Instructors</Link></li>
+          <li><Link to='/'>Classes</Link></li>
+        </ul>
+      </div>
+
+      <div>
+        {user
+          ?
+          <div className='flex items-center justify-center'>
+            <img className='h-16 w-16 bg-teal-700 rounded-full' src={logo} alt="" />
+            <Link to='/'>
+              <button className="btn btn-accent btn-outline btn-sm mx-2">Dashboard</button>
+            </Link>
+            <button className="btn btn-error btn-outline btn-sm">Logout</button>
+          </div>
+          :
+          <Link to='/'>
+            <button className="btn btn-accent btn-outline btn-sm">Login</button>
+          </Link>
+        }
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
