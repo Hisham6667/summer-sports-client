43e620379d2d9bcbdb17240b5377f411a4103959
Hisham6667 <hisham.44678@gmail.com>
2023-06-11 21:34:30 +0600

    instructor page complted

--- src/Pages/Instructors/InstructorCard.jsx
+++ src/Pages/Instructors/InstructorCard.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
 
-const InstructorCard = ({instructor}) => {
-  console.log(instructor);
+const InstructorCard = ({ instructor }) => {
+
+  const { name, email, photoURL } = instructor;
+
   return (
-    <div>
-      
+    <div className="card card-side glass">
+      <figure className='border-4 border-teal-300 w-60'>
+        <img className='w-full h-full' src={photoURL} alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <p className="card-title text-3xl text-center uppercase">{name}</p>
+        <p className='text-lg'>{email}</p>
+      </div>
     </div>
   );
 };
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -11,13 +11,15 @@ const Instructors = () => {
       <span className="loading loading-spinner text-accent w-14"></span>
     </div>
   }
-  
+
   return (
-    <div>
-      <p className="text-4xl font-bold p-8 border border-teal-300 text-center rounded-full shadow-xl bg-amber-600/20 font-serif uppercase mb-10">Our multi talented {instructors.length} best Instructors</p>
-      {
-        instructors.map(instructor => <InstructorCard key={instructor._id} instructor={instructor}></InstructorCard>)
-      }
+    <div className="w-full">
+      <p className="text-4xl font-bold p-8 border border-teal-300 text-center rounded-full shadow-xl bg-amber-600/20 font-serif uppercase mt-7 mb-20">Our multi talented {instructors.length} best Instructors</p>
+      <div className="grid grid-cols-2 gap-10 p-10 mb-20" style={{backgroundImage: "url(https://i.ibb.co/S0zVKdT/instructors-bg.jpg)"}}>
+        {
+          instructors.map(instructor => <InstructorCard key={instructor._id} instructor={instructor}></InstructorCard>)
+        }
+      </div>
     </div>
   );
 };
