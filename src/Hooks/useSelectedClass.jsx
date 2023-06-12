ac7d314edc70ca14bcc17bca9230e4a3e74430be
Hisham6667 <hisham.44678@gmail.com>
2023-06-12 12:46:31 +0600

    useSelectedClasses hook added

--- src/Hooks/useSelectedClass.jsx
+++ src/Hooks/useSelectedClass.jsx
@@ -1,13 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useSelectedClass = () => {
 
-  
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
 
-  return (
-    <div>
-      
-    </div>
-  );
+  const { data: selectedClasses = [], refetch } = useQuery({
+    queryKey: ['selectedClasses', user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure(`/selectedclasses?email=${user?.email}`)
+      return res.data
+    }
+  })
+
+  return [selectedClasses, refetch]
 };
 
 export default useSelectedClass;
